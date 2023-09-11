0760715f8aa5d0a215d8e8c35f4e6c1cc9777ed5
buckapi <buckapis@gmail.com>
2023-09-10 20:10:54 -0500

    SHOPS

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_NAMED_OPTIONS,NamedOptions} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'https://db.corpcssca.com:4015'; // <-- add the URL of the GraphQL server here
+const uri = '--'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
@@ -32,7 +32,7 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
           openimport: /* <-- these settings will be saved by name: newClientName */ {
             cache: new InMemoryCache(),
             link: httpLink.create({
-              uri: 'https://db.corpcssca.com:4015',
+              uri: '--',
             }),
           },
         };
